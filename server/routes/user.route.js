020196b14a87829bcf585d2742639210e68a8b75
Imad Ouibourk <imad.ouibourk@coding-academy.fr>
2019-08-29 18:31:54 +0200

    modify user.route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,4 +4,9 @@ module.exports = function (app) {
     app.post('/login',UserController.login);
     app.post('/signup',UserController.signup);
     app.get('/listUser',UserController.listUser);
+    app.get('/listUser/:id',UserController.DetailsUser);
+    app.put('/listUser/:id/update',UserController.UpdateUser);
+    app.delete('/listUser/:id/delete',UserController.DeleteUser);
+    
+    
 }
