bfb19d4a240953bc20938b10e450d0dc1bbb1807
Imad Ouibourk <imad.ouibourk@coding-academy.fr>
2019-08-29 15:55:47 +0200

    modify user.route.js

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,6 +1,7 @@
-const account = require('../controllers/user.controller.js');
+const UserController = require('../controllers/user.controller.js');
 
 module.exports = function (app) {
-    app.post('/login',account.login);
-    app.post('/signup',account.signup);
+    app.post('/login',UserController.login);
+    app.post('/signup',UserController.signup);
+    app.get('/listUser',UserController.listUser);
 }
