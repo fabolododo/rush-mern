12f76ec8b09981ff0d82d8ac646f4daacb76b09e
fabolododo <fabolododo@hotmail.fr>
2019-09-05 11:11:47 +0200

    added post.route.js

---
+++ server/routes/post.route.js
@@ -0,0 +1,9 @@
+const PostController = require('../controllers/post.controller.js');
+
+module.exports = function (app) {
+    app.post('/addPost',PostController.addPost);
+    // app.get('/listPost',PostController.listPosts);
+    // app.get('/listPost/:userId',PostController.UserPosts);
+    // app.put('/listPost/:idPost/update',PostController.UpdatePost);
+    // app.delete('/listPost/:idPost/delete',PostController.DeletePost);
+}
