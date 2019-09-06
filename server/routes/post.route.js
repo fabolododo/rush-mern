cb614d11b1c77c575387b1d84d1a9ff3a98e5d56
fabolododo <fabolododo@hotmail.fr>
2019-09-06 18:39:49 +0200

    modified post route

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -2,8 +2,8 @@ const PostController = require('../controllers/post.controller.js');
 
 module.exports = function (app) {
     app.post('/addPost',PostController.addPost);
-    app.get('/listPost',PostController.listPosts);
-    // app.get('/listPost/:userId',PostController.UserPosts);
-    // app.put('/listPost/:idPost/update',PostController.UpdatePost);
-    // app.delete('/listPost/:idPost/delete',PostController.DeletePost);
+    app.get('/listPosts',PostController.listPosts);
+    // app.get('/listPost/:id',PostController.UserPosts);
+    // app.put('/listPost/:id/update',PostController.UpdatePost);
+    // app.delete('/listPost/:id/delete',PostController.DeletePost);
 }
